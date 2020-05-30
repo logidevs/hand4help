0be5efed30db7237484d3e145b3fa7bc5499e6d7
BlueSilver001 <dubljanin96@gmail.com>
2020-05-30 16:37:32 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,93 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.front')
+@section('css')
+<link href="{{ asset('css/front.css') }}" rel="stylesheet">
 
-        <title>Laravel</title>
+@endsection
+@section('front.content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<header>
+    <div class="overlay"></div>
+    <div class="container h-100">
+        <div class="d-flex h-100 text-center align-items-center">
+            <div class="w-100 text-white">
+                <h1 class="display-2">Help yourself by helping others
+                </h1>
+                <p class="h1">Help those that need help in order to get help that you will need some day. BE HUMANE</p>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</header>
+<div class="container my-5 text-center">
+    <div class="row ">
+        <div class="col-md-6">
+            <h2>Do you need help or are you know someone that need help?</h2>
+            <a href="{{route('help')}}" class="btn btn-danger py-3 px-4 btn-lg text-white">Ask for assistance</a>
+        </div>
+        <div class="col-md-6">
+            <h2>Are you willing to help others?<br><br></h2>
+            <button type="button" class="btn btn-danger py-3 px-4 btn-lg text-white">Register for volunteering</button>
+        </div>
+    </div>
+</div>
+<div class="container-fluid ">
+    <div class="row mb-5 ">
+        <div class="col-md-4 bg-primary text-white p-5">
+            <h2 class="pt-4 pb-3">The long journey to end Covid19 begins with a humanity.</h2>
+            <button class="btn btn-outline-danger font-weight-bold btn-lg">Help now!</button>
+        </div>
+
+        <div class="col-md-4 bg-secondary text-white  p-5">
+            <h2 class="pt-4 pb-3">
+                During this unprecented time, many people use your help.</h2>
+            <button class="btn btn-outline-danger font-weight-bold btn-lg">Get help!</button>
+        </div>
+        <div class="col-md-4 p-0"><img src="{{asset('uploads/adventure.jpg')}}" class="img-fluid" alt=""></div>
+    </div>
+
+
+
+    <h1 class="text-secondary text-center mb-5">Our Causes!</h1>
+
+
+
+    <div class="row mb-5">
+        <div class="col-md-6 bg-primary text-white p-5">
+            <h3>Why Was HandsForHelp made?</h3><br>
+            <p class="lead">
+                During the response to COVID-19,
+            </p>
+
+
+            </p>
+        </div>
+        <div class="col-md-6 p-0"><img src="{{asset('uploads/v.jpg')}}" class="img-fluid" alt=""></div>
+        <div class="col-md-6 p-0 d-none d-lg-block"><img src="{{asset('uploads/handshake.png')}}" class="img-fluid"
+                alt="">
+        </div>
+        <div class="col-md-6 bg-secondary text-white p-5">
+            <h3>
+                How Does it Work?</h3><br>
+            <p class="lead">
+                Let's get one thing staint, it is free of charge! We match those
+                in help with
+                volunteers who have donated theres time
+            </p>
+        </div>
+    </div>
+</div>
+
+    <footer class="p-5 bg-dark text-white"><div class="container">
+        <div class="row  ">
+            <div class="col-md-6">CONTACT INFO<br>
+            Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit,<br> sed do eiusmod tempor incididunt<br>
+         office@logidevs.com
+
+            </div>
+        <div class="col-md-6">QUICK LINKS
+        </div></div></div>
+    </footer>
+    <div class="text-center py-4 bg-secondary text-white-50">
+        <small>Copyright &copy; LogiDevs.com</small>
+    </div>
+
+    @endsection
